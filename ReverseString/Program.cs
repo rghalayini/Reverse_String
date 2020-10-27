ff136b032a48d4563ae489de295db475cf55e3ed
Remy Ghalayini <remyghalayini@gmail.com>
2020-10-27 11:28:20 +0100

    first commit

---
+++ ReverseString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reverse string application");
+            Console.Write("Insert word or sentence: ");
+            string insertedWord = Console.ReadLine();
+
+            char[] wordArray = insertedWord.ToCharArray();
+            Array.Reverse(wordArray);
+
+            Console.WriteLine(wordArray);
+            Console.ReadLine();
+
+        }
+    }
+}
